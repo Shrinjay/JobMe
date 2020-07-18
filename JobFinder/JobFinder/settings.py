be64ea98e8dcb5a8c2704219c3814e9a23343908
Shrinjay Mukherjee <62576642+Shrinjay@users.noreply.github.com>
2020-07-18 11:09:01 -0600

    Update settings.py

--- JobFinder/JobFinder/settings.py
+++ JobFinder/JobFinder/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bkre4aspea91!sxh@5pr0s6*@+1m$!cmrp-pd4$^89*q!s6t@s'
+SECRET_KEY = 'Insert key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
